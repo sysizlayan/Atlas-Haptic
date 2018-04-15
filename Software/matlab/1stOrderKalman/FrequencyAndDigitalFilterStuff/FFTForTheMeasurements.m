d118a9f2b6488436319dc1641ba9a588e9c683ec
sysizlayan <sysizlayan@gmail.com>
2018-04-15 10:19:07 +0300

    after TORK

--- Software/matlab/1stOrderKalman/FrequencyAndDigitalFilterStuff/FFTForTheMeasurements.m
+++ Software/matlab/1stOrderKalman/FrequencyAndDigitalFilterStuff/FFTForTheMeasurements.m
@@ -7,13 +7,21 @@ global t
 
 Fs = 1/dt;
 Y= fft(diff(angPos)./dt);
+Y1 = fft(gyro_angVel);
+P2 = abs(Y1/N);
 
-P2 = abs(Y/N);
 P1 = P2(1:floor(N/2)+1);
 P1(2:end-1) = 2*P1(2:end-1);
 f_vector= Fs*(0:floor(N/2))/N;
-figure
+experiment = figure;
 plot(f_vector,P1);
 xlabel('Frequency (Hz)');
-ylabel('Power Spectrum')
+ylabel('Power Spectrum');
+xlim([0,500])
 title(strcat('Frequency Power Spectra of Measurement #',num2str(measurement_number)));
+
+set(experiment,'PaperPositionMode','auto');         
+set(experiment,'PaperOrientation','landscape');
+set(experiment,'Position',[50 50 1200 800]);
+print(experiment, '-dpdf', strcat('./Figures/measurement_',num2str(measurement_number),'__fft_gyro.pdf'))
+
--- Software/matlab/1stOrderKalman/FrequencyAndDigitalFilterStuff/filterTheDiff.m
+++ Software/matlab/1stOrderKalman/FrequencyAndDigitalFilterStuff/filterTheDiff.m
@@ -5,25 +5,24 @@ global filtered_angPos
 global gyro_angVel
 global t
 Fs = 1/dt;
-[bButter,aButter] = butter(6,20*pi/Fs);
+[bButter,aButter] = butter(2,20*pi/Fs);
 [bFIR,aFIR] = fir1(10,20*pi/Fs);
 filteredDifferenceButter = filter(bButter,aButter,diff(angPos)./dt);
 filteredDifferenceFIR = filter(bFIR,aFIR,diff(angPos)./dt);
-filterOut = figure
+filterOut = figure;
 % plot(t(2:end),filteredDifferenceButter)
 % hold on
 plot(t,gyro_angVel)
 hold on
-plot(t(2:end),filteredDifferenceFIR);
-legend('Gyro Measurement','FIR Filtered Difference');
+plot(t(2:end),filteredDifferenceButter);
 xlim([2,3])
 ylim([100,450])
-legend('Butter Filtered Difference','Gyro Measurement');%,'FIR filtered Difference');
-xlabel('Time(s)');
-ylabel('Angular Velocity');
-
+legend('20 Hz Kesme Frekanslý Butterworth Filtresi','Jiroskop Ölçümü');%,'FIR filtered Difference');
+xlabel('Zaman(s)');
+ylabel('$$\dot{\theta} (^\circ /s)$$','Interpreter','latex');
+title('Filtrelenmiþ Geriye Dönük Hýz Kestirimi ve Jiroskop Karþýlaþtýrmasý');
 set(filterOut,'PaperPositionMode','auto');         
 set(filterOut,'PaperOrientation','landscape');
 set(filterOut,'Position',[50 50 1200 800]);
-print(filterOut, '-dpdf', strcat('filteredOutput.pdf'))
+print(filterOut, '-dpdf', strcat('butter.pdf'))
 
